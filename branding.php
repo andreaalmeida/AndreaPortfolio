07f6fcc3fc673897c11f825335f29ce82ee92297
Andrea PeAlmeida <andreaalmeida@me.com>
2016-06-09 12:55:43 -0400

    google analytics

--- branding.php
+++ branding.php
@@ -1,14 +1,24 @@
 <!DOCTYPE HTML>
 <html lang="en">
-<head>
-  	<meta charset="UTF-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  	<title>Fooda Branding - Andrea Pereira de Almeida</title>
-  	<link rel="stylesheet" href="stylesheets/styles.css">
-  	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
-  	<link href='https://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
-</head>
+	<head>
+	  	<meta charset="UTF-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	  	<title>Fooda Branding - Andrea Pereira de Almeida</title>
+	  	<link rel="stylesheet" href="stylesheets/styles.css">
+	  	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
+	  	<link href='https://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
+	</head>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-49710728-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 	<body>
 		<?php include 'includes/header.php' ?>
 		<div class="index_start">
--- contact.php
+++ contact.php
@@ -11,6 +11,16 @@ $current = 'contact';
 	  	<link rel="stylesheet" href="stylesheets/styles.css">
 	  	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
 	</head>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-49710728-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 	<body>
 		<?php include 'includes/header.php'; ?>
 		<div class="index_start">
@@ -27,7 +37,7 @@ $current = 'contact';
 		<div class="description">
 			<div class="description__container--contact">
 				<h3>Andrea Pereira de Almeida</h3>
-				<p>User Experience Designer</br>Chicago, IL</p>
+				<p>User Experience Designer</br>Ann Arbor, MI</p>
 				<h3>Email</h3>
 				<p><a class="mail" href="mailto:andreaalmeida@me.com?Subject=Hey%20Andrea" target="_top">andreaalmeida@me.com</a>
 				</p>
