54863827a941d6bbe3f0de759e19a9b41536f020
Andrea PeAlmeida <andreaalmeida@me.com>
2016-06-17 12:54:30 -0400

    Bitovi Cover Letter

---
+++ bitovi.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	  <title>Bitovi Cover Letter - Andrea Pereira de Almeida</title>
+	  <link rel="stylesheet" href="stylesheets/styles.css">
+	  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
+	</head>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-49710728-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+	<body>
+		<?php include 'includes/header.php'; ?>
+		<div class="index_start">
+		</div>
+		<div class="pageTitle--bitovi">
+			<div class="pageTitle__container">
+				<h1 class="h1_white">A letter to Bitovi</h1>
+				<p class="p_white">Why you should interview me for a UX position on your team</p>
+			</div>	
+		</div>
+		<div class="results--bitovi">
+			<div class="results__container">
+				<p class="paragraph">Dear Tom,</p>
+				<p>I read your post, <a href="http://blog.bitovi.com/4-values-make-design-team-different/" class="mail" target="_blank">The four values that make Bitovi’s design team different</a>, and felt compelled to apply for this role after doing so. I think that I would be a great fit as a UX designer on your team, and here is why: 
+				</p>
+				<h3>I'm a clear communicator and a great listener</h3>
+				<p class="paragraph">During my last job as a UX Designer at <a href="http://www.fooda.com" class="mail" target="_blank">Fooda</a>, I gave weekly presentations to our board of executives to explain my designs. Although this was initially intimidating, I became increasingly comfortable discussing my decision-making process in a logical and concise way. 
+				</p>
+				<p class="paragraph">Since leaving Fooda, I’ve been focusing on improving my writing style. I sought out the best ways to improve and landed on a book called <a href="http://www.goodreads.com/book/show/1552699.Revising_Prose" class="mail" target="_blank">Revising Prose</a> by Richard A. Lanham. Along with some other resources, the book taught me how to write both more clearly and more authentically. See any of the writeups in my portfolio for some <a href="index.php" class="mail" target="_blank">examples</a>.
+				</p>
+				<p class="paragraph">While public speaking and writing are skills that I’ve had to consciously work on over the past few years, I’ve always been a great listener. Listening is one of my strengths as a designer because it allows me to naturally connect with my clients and better understand their needs and perspectives.
+				</p>
+				<p>My ability to listen deeply and express myself clearly in both spoken and written form will be especially valuable on a team like Bitovi’s, where the work is 100% remote.
+				</p>
+			
+			</div>
+			<div class="results__container">
+				<h3>I enjoy coding and working with developers</h3>
+				<p class="paragraph">I’ve been using HTML and CSS since 2009 and I’m confident that my skills are well above average. While I still use these tools, I’ve progressively realized that I’m happiest and most engaged when I’m learning to create in new ways. My portfolio is coded using HTML, CSS, SASS, and a little PHP. Although I just started teaching myself PHP, I decided to incorporate it into my portfolio so that I could get some practice with the syntax. I also installed MAMP on my laptop to test my site locally before pushing my code through the command line to GitHub.  
+				</p>
+				<p>Because I enjoy coding so much, my best relationships at my last company were naturally with the developers. We were always on the same wavelength and excited to tackle problems together in creative ways. Wherever I work next, I hope it is with a company that gives me plenty of opportunities to learn from and grow with developers. Bitovi seems to fit that description.
+				</p>
+			</div>
+			<div class="results__container">
+				<h3>I’m excited about the work you do at Bitovi and our values seem to align</h3>
+				<p class="paragraph">From working on projects with huge companies like Walmart and Sam’s Club to collaborating with nonprofits, it seems like you guys are working on a lot of exciting and meaningful problems.
+				</p>
+				<p class="paragraph">Based on the way the content is written on Bitovi’s site, as well as the values and skills that you emphasize in your post, I think I'd fit in very well with your team. If you are still hiring and you like my work, I'd be excited to talk with you more in depth. 
+				</p>
+
+			</div>
+			<div class="results__container">
+				<p class="paragraph">Thanks for your time,</br>
+					Andrea Pereira de Almeida</p>
+			</div>
+			<div class="projectNav"></div>
+		</div>
+	</body>
+	<?php include 'includes/footer.php'; ?>
+</html>
